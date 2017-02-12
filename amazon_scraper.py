da0e9b2bff67ea3f3e4fff024dbe9fef0ed2efaf
Mary Ruth Ngo <mngo@wellesley.edu>
2017-02-11 20:12:15 -0500

    finished amazon product web scraper

--- amazon_scraper.py
+++ amazon_scraper.py
@@ -1,24 +1,74 @@
 from bs4 import BeautifulSoup
 import urllib
+import re
 
-r = urllib.urlopen('https://www.amazon.com/ORVILLE-REDENBA-\
-					Natural-Microwave-Popcorn/dp/B008V9997C/\
-					ref=sr_1_1_a_it?ie=UTF8&qid=1486851122&sr=\
-					8-1&keywords=popcorn+orvills').read()
+#example
+products = ['https://www.amazon.com/Highlands-Argyle-Mens-Golf-Collection/dp/B00EDQW9G0/ref=sr_1_50?ie=UTF8&qid=1486852744&sr=8-50-spons&keywords=socks&psc=1',
+				'https://www.amazon.com/Annies-Macaroni-Cheese-Microwave-Cheddar/dp/B00D7D1Y7U/ref=sr_1_1?ie=UTF8&qid=1486850821&sr=8-1-spons&keywords=annie%27s+mac+and+cheese&psc=1',
+				'https://www.amazon.com/dp/B00RNEBB90/ref=twister_B00RNEBB6S?_encoding=UTF8&th=1',
+				'https://www.amazon.com/dp/B01D2ZN5LK/ref=twister_B01HTRXLB6?_encoding=UTF8&psc=1'
+				]
 
+def build_product(url):
+	product = {}
+	r = urllib.urlopen(url).read()
 
-soup = BeautifulSoup(r, 'html.parser')
+	soup = BeautifulSoup(r, 'html.parser')
 
-# data = soup.prettify()
-data = soup.find_all('div', class_='imgTagWrapper')
+	####################### Image Scraping #####################
+	img_data = soup.find_all('div', class_='imgTagWrapper')
 
-for tag in data:
-	# print tag.name, '\n'
-	for t in tag:
-		# print t.name
-		if t.name == 'img':
-			# print t.attrs
-			print t['src']
-# print '\n'.join(data)
-# print type(data)
-# print data
+	for tag in img_data:
+		for t in tag:
+			if t.name == 'img':
+				# print t['src']
+				product['image'] = t['src']
+
+	####################### Title Scraping #####################
+	title_data = soup.find_all('span', id='productTitle')
+	
+	if len(title_data) > 1:
+		print 'TITLE DATA IS LENGTH: ', len(title_data)
+	
+	for tag in title_data:
+		title = tag.contents[0].strip()
+		product['title'] = title
+		# print title
+
+	pack_of = title.split('Pack of')
+	if len(pack_of) == 1:
+		#no packs - just product with 1 unit
+		pack_of = 1
+	else:
+		# clean result
+		pack_of = pack_of[1].split(")")[0].strip().split(' ')[0]
+	# print pack_of
+	product['pack_of'] = str(pack_of)
+
+	####################### Price Scraping #####################
+	price_data = soup.find_all('div', id='price_feature_div')[0]
+
+	# CASE 1: product with sale
+	snsPrice = price_data.find_all('div', id='snsPrice')
+	price = []
+	if len(snsPrice) > 0:
+		for s in snsPrice:
+			spans = s.find_all('span', class_=re.compile('price'))
+			for sp in spans:
+				price.append(sp.contents[0].strip())
+			# print 'PRIME PRODUCT with sale: ', ' '.join(price)
+	else:
+		# CASE 2: produce with no sale
+		primePrice = soup.find_all('div', id='price_feature_div')
+		for s in primePrice:
+			trs = s.find_all('span', id=re.compile('ourprice'))
+			for s in trs:
+				price.append(s.contents[0].strip())
+			# print 'PRIME PRODUCT no sale: ', ' '.join(price)
+
+	product['price'] = price
+	####################### END Price Scraping #####################
+	return product
+
+for url in products:
+	print build_product(url)

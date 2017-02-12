0c04ba2916ff8815e00c4d09da1379a76633e7d4
Mary Ruth Ngo <mngo@wellesley.edu>
2017-02-12 10:29:25 -0500

    move out of website directory

---
+++ sqlalchemy-workspace/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/maryruthngo/Desktop/coshop/website/sqlalchemy-workspace/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

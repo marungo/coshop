6601e66841326ef647be7eaef709286ae4e8b322
Mary Ruth Ngo <mngo@wellesley.edu>
2017-02-12 10:49:44 -0500

    no sqlalchemy pls

--- sqlalchemy-workspace/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/maryruthngo/Desktop/coshop/website/sqlalchemy-workspace/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()

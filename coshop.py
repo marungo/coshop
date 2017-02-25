4c22f534d4c5f15e7f2bbe186036c8220d4bc1d4
Mary Ruth Ngo <mngo@wellesley.edu>
2017-02-25 10:43:02 -0500

    committing to pull

--- coshop.py
+++ coshop.py
@@ -79,14 +79,6 @@ def build_product(url):
 	####################### END Price Scraping #####################
 	return product
 
-@APP.route('/')
-def index():
-    """ Displays the index page accessible at '/'
-    """
-    build_product('https://www.amazon.com/dp/B01D2ZN5LK/ref=twister_B01HTRXLB6?_encoding=UTF8&psc=1')
-    return flask.render_template('index.html')
-
-
 @APP.route('/', methods=['POST'])
 def my_form_post():
     print "inside!"

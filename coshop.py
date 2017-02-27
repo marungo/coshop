1dba4d20059b1530a9a61d18a6124dfbf5eecb03
Mary Ruth Ngo <mngo@wellesley.edu>
2017-02-26 22:07:59 -0500

    clean up files

--- coshop.py
+++ coshop.py
@@ -1,71 +1,19 @@
 #!/usr/bin/env python
-
 import flask
 from flask import request
 from flask_sqlalchemy import SQLAlchemy
 
 import os
-from bs4 import BeautifulSoup
-import urllib
-import re
-
-#coshop db
-# import coshopdb
 import load_products
 
 
 # Create the application.
 APP = flask.Flask(__name__)
 APP.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://zkuzqmqystamvh:972956f846e943664bf64b437c0628f9518dda2d1616abd7f053a3bcfe373bf8@ec2-184-72-249-88.compute-1.amazonaws.com:5432/da1iad07bev1ji'
+APP.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(APP)
 
-############ DATABASE ###############
-commits = db.Table('commits',
-	db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
-	db.Column('prod_id', db.Integer, db.ForeignKey('product.asin'))
-)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80))
-    email = db.Column(db.String(120), unique=True)
-    # products = db.relationship('Product', secondary=commits, 
-    # 				backref=db.backref('user',lazy='select'), lazy='select')
-
-    def __init__(self, name, email):
-        self.name = name
-        self.email = email
-
-    def __repr__(self):
-        return '<Name %r>' % self.name
-
-
-class Product(db.Model):
-    asin = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200))
-    price = db.Column(db.String(10))
-    unit_price = db.Column(db.String(10))
-    pack_of = db.Column(db.String(10))
-    # users = db.relationship('User', backref=db.backref('product',
-    # 							lazy='dynamic'))
-
-
-    def __init__(self, title, price, pack_of):
-        self.title = title
-        self.price = price
-        self.pack_of = pack_of
-
-    def __repr__(self):
-        return '<Title %r>' % self.title
-
-def add_product_to_db(product):
-	prod = Product(asin=product['asin'], title=product['title'],
-					price=product['price'][0], unit_price=product['unit_price'],
-					pack_of=product['pack_of'])
-	db.session.add(prod)
-	db.session.commit()
-
-############ END DATABASE ###############
+import models
 
 
 @APP.route('/')
@@ -74,22 +22,6 @@ def index():
     """
     #build_product('https://www.amazon.com/dp/B01D2ZN5LK/ref=twister_B01HTRXLB6?_encoding=UTF8&psc=1')
     return flask.render_template('index.html')
-
-# @APP.before_request
-# def get_products():
-	# products = load_static_products(os.getcwd() + '/static')
-
-# @APP.route('/getasin', methods=['GET', 'POST'])
-# def index():
-#     """ Displays the index page accessible at '/'
-#     """
-#     #build_product('https://www.amazon.com/dp/B01D2ZN5LK/ref=twister_B01HTRXLB6?_encoding=UTF8&psc=1')
-#     url = flask.request.form['amazonProduct']
-# 	# get Asin from url
-# 	r = re.compile("(?<=/dp/).*(?=/)")
-# 	global product_asin
-# 	product_asin = r.findall(url)[0]
-#     return my_form_post(product_asin)
 
 
 @APP.route('/form', methods=['GET', 'POST'])
---
+++ models.py
@@ -0,0 +1,54 @@
+from coshop import db
+from sqlalchemy.dialects.postgresql import JSON
+
+
+commits = db.Table('commits',
+	db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+	db.Column('prod_id', db.Integer, db.ForeignKey('product.asin'))
+)
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    email = db.Column(db.String(120), unique=True)
+    # products = db.relationship('Product', secondary=commits, 
+    # 				backref=db.backref('user',lazy='select'), lazy='select')
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
+
+class Product(db.Model):
+    __tablename__ = 'products'
+    
+    asin = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200))
+    price = db.Column(db.String(10))
+    unit_price = db.Column(db.String(10))
+    pack_of = db.Column(db.String(10))
+    # users = db.relationship('User', backref=db.backref('product',
+    # 							lazy='dynamic'))
+
+
+    def __init__(self, title, price, pack_of):
+        self.title = title
+        self.price = price
+        self.pack_of = pack_of
+
+    def __repr__(self):
+        return '<Title %r>' % self.title
+
+def add_product_to_db(product):
+	prod = Product(asin=product['asin'], title=product['title'],
+					price=product['price'][0], unit_price=product['unit_price'],
+					pack_of=product['pack_of'])
+	db.session.add(prod)
+	db.session.commit()
+
+############ END DATABASE ###############
